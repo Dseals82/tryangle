8a92d23085c7deced0055b0d6b69b1fe6321e026
Darius Seals <tristarmediagroupinc@gmail.com>
2018-08-04 01:48:45 -0400

    refactored code and added to tests, also, added a method to describe what kind of triangle cannot exist

--- lib/tryangle.rb
+++ lib/tryangle.rb
@@ -1,10 +1,29 @@
-def isTriangle(a, b, c)
-if a == b && a==c
-  "Equilateral"
-elsif
-  a == b && a != c or b == c or a == c
-  "Isosceles"
-else
-  "Scalene"
+
+def isTriangle(a,b,c)
+  if isEquilateral(a,b,c)
+    "Equilateral"
+  elsif isIsosceles(a,b,c)
+    "Isosceles"
+  elsif isScalene(a,b,c)
+    "Scalene"
+  else isNotTriangle(a,b,c)
+  end
 end
+# I wish I could put this lower half of code in a partial... note to self: check for solution
+def isNotTriangle(a,b,c)
+  if a > b && c or b > a && c or c > a && b
+    "Triangle Cannot Exist"
+  end
+end
+
+def isEquilateral(a,b,c)
+  a == b && a==c
+end
+
+def isIsosceles(a,b,c)
+  a == b && a != c or b == c or a == c
+end
+
+def isScalene(a,b,c)
+  a != b && c or b != a && c or c != a && b
 end
--- spec/tryangle_spec.rb
+++ spec/tryangle_spec.rb
@@ -1,9 +1,9 @@
+
 require_relative '../lib/tryangle'
 
 RSpec.describe "isTriangle" do
   context 'Equilateral' do
     it "should return Equilateral when all three sides are equal" do
-
       expect(isTriangle(3,3,3)).to eq("Equilateral")
     end
   end
@@ -17,5 +17,12 @@ RSpec.describe "isTriangle" do
       it "should return Scalene when no sides are equal to eachother" do
         expect(isTriangle(1,2,3)).to eq("Scalene")
       end
+    end
+
+    context 'isNotTriangle' do
+      it "Should return Triangle cannot exist when one of the sides are greater
+                                                          than the other two" do
+        expect(isNotTriangle(20,30,100)).to eq("Triangle Cannot Exist")
+      end
+    end
   end
-end
